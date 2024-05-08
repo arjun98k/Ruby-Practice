60e36d480c521abf7ab3164484a3be05fa77fbb9
arjun98k <arjunkandekar14@gmail.com>
2024-05-08 06:10:05 +0530

    add file to github

--- Operator_ruby.rb
+++ Operator_ruby.rb
@@ -1,1 +1,18 @@
-puts 1 + 1
+# a = 10 
+# b = 20
+# c =5
+# d =2
+
+# puts a + b 
+# puts b - a 
+# puts a * b 
+# puts c ** d  
+# puts b%a 
+
+
+# spaceship operator
+
+person_1 = 98
+person_2 = 98
+
+puts person_1 <=> person_2
