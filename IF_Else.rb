27f8471d74650477025ceaf36ce0a1e4f4641e8a
arjun98k <arjunkandekar14@gmail.com>
2024-05-08 23:33:01 +0530

    add file to github

---
+++ IF_Else.rb
@@ -0,0 +1,14 @@
+begin
+puts "enter your number"
+x = gets.chomp.to_i
+if x > 98 
+    puts "he is not an arjun kandekar"
+elsif x < 98
+    puts "he is not an arjun kandekar"    
+  
+elsif x == 98
+    puts "he is arjun kandekar"
+  
+end
+ 
+end
