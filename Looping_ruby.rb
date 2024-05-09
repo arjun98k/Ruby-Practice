369dab8f54c35197ac59403d880ad79b9f4f06ab
arjun98k <arjunkandekar14@gmail.com>
2024-05-09 11:19:00 +0530

    add file to github

---
+++ Looping_ruby.rb
@@ -0,0 +1,16 @@
+
+# while loop
+
+# a = 1
+# while  a < 10 do
+#     puts ("arjun forever in love in coding")
+#     a +=1
+  
+# end
+
+
+for a in 1..9
+    puts "shri is first love #{a}"
+  
+end
+
